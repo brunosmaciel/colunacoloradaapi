a78721dc492e865b586d8b84e0308567ad5050e0
Bruno Maciel <brunosazanamaciel@gmail.com>
2022-08-10 10:59:26 -0300

    Tentativa de correcao do bug de deploy

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,7 @@ mongoose.connect(connectionString).then(() => {
   app.emit('connected');
   console.log('Conecting...');
 });
-const port = process.env.APP_PORT || 3000;
-
+const port = process.env.PORT || 3000;
 app.on('connected', () => {
   app.listen(port, () => {
     console.log(`Conectend`);
